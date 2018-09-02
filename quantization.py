7cda055c188ca60de2b063dcfb93536212781e21
João Vitor Elias Carvalho <joaovec@gmail.com>
2018-09-02 00:53:52 -0300

    Add quantization module and simple unit test

---
+++ quantization.py
@@ -0,0 +1,46 @@
+import math
+import time
+
+from PIL import Image as pil_image
+import numpy as np
+import cv2
+
+
+def quantize_lab_image(lab_image, bins, max_value):
+    # type: (np.ndarray, int) -> np.ndarray
+
+    ab_channels = lab_image[:, :, 1:]
+
+    division_factor = math.ceil(max_value / bins)
+
+    ab_channels = (ab_channels // division_factor) - 1
+    ab_channels[:, :, 0] = ab_channels[:, :, 0] * bins
+
+    indexes = ab_channels[:, :, 0] + ab_channels[:, :, 1]
+
+    image_shape = lab_image.shape
+
+    final_result = np.zeros(image_shape + (bins**2,))
+
+    for i in range(image_shape[0]):
+        for j in range(image_shape[1]):
+            index = int(indexes[i, j])
+            pixel_distribution = np.zeros(bins**2)
+            pixel_distribution[index] = 1.0
+            final_result[i, j] = pixel_distribution
+
+    return final_result
+
+
+def convert_quantization_to_image(quantization, bins):
+    # type: (np.ndarray, int) -> np.ndarray
+    image_shape = (quantization.shape[0], quantization.shape[1])
+
+    indexes = np.argmax(quantization[:, :, :], axis=3)[:, :, 0]
+    a_channel = (indexes // bins) + 1
+    b_channel = (indexes % bins) + 1
+
+    a_channel = a_channel.reshape(image_shape[0], image_shape[1], 1)
+    b_channel = b_channel.reshape(image_shape[0], image_shape[1], 1)
+
+    return np.concatenate((a_channel, b_channel), axis=2)
---
+++ quantization_test.py
@@ -0,0 +1,24 @@
+import unittest
+
+import numpy as np
+
+from quantization import convert_quantization_to_image, quantize_lab_image
+
+
+class TestQuantization(unittest.TestCase):
+
+    def test_small_array(self):
+        image = np.ones((10, 10, 3)) * 10.
+
+        quantum = quantize_lab_image(image, 255, 255)
+        final_image = convert_quantization_to_image(quantum, 255)
+
+        l_channel = image[:, :, 0].reshape(image.shape[0], image.shape[1], 1)
+        a_channel = final_image[:, :, 0].reshape(image.shape[0], image.shape[1], 1)
+        b_channel = final_image[:, :, 1].reshape(image.shape[0], image.shape[1], 1)
+
+        final_image = np.concatenate((l_channel, a_channel, b_channel), axis=2)
+
+        diff = image - final_image
+        all_zeros = not np.any(diff)
+        self.assertTrue(all_zeros)
