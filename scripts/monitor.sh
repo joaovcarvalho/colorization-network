a62bd619679c4a2e6cbb5e5d9ca1c7c574193d30
João Vitor Elias Carvalho <joaovec@gmail.com>
2018-09-02 22:15:02 -0300

    Add continuing monitoring to monitor script

--- scripts/monitor.sh
+++ scripts/monitor.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
-sshpass -p '$@Jv.21301410' scp -P 25252 -r jcarvalho@tesla.dcc.ufrj.br:imagenet_train.out imagenet_train.out
-tail imagenet_train.out
+
+while :
+do
+    printf '\n Running... \n'
+    sshpass -p '$@Jv.21301410' scp -P 25252 -r jcarvalho@tesla.dcc.ufrj.br:imagenet_train.out imagenet_train.out
+    tail imagenet_train.out
+	sleep 2
+done
