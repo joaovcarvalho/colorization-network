0342b676f3d63de025ba3203af2bdac23f932b2b
João Carvalho <joaovec@gmail.com>
2019-08-31 17:09:31 -0300

    Add plot loss py script

---
+++ plot_loss.py
@@ -0,0 +1,19 @@
+import re
+import matplotlib.pyplot as plt
+import numpy as np
+
+file = open('loss.out', 'r')
+lines = [
+            re.sub("[^0-9]", "", line) for line in file.readlines()
+            ]
+
+lines = [line for line in lines if line != '']
+
+numbers = map(float, lines)
+data = np.array(numbers)
+data = data / np.max(data)
+
+plt.plot(data)
+plt.ylabel('loss')
+plt.show()
+
